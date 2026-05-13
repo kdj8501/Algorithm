8b94afdec988e6d86e46c0293036af5adf6be221
구동재 <68319028+kdj8501@users.noreply.github.com>
2026-05-13 16:29:42 +0900

    [Passed] 원 모양으로 되어있는 방 (209ms, 13MB)

---
+++ 260513/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (i > j) {
+                    sum += (n - (i - j)) * arr[j];
+                } else {
+                    sum += (j - i) * arr[j];
+                }
+            }
+            min = Math.min(min, sum);
+        }
+        System.out.println(min);
+    }
+}
